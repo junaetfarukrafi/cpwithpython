977ce45f2339b0e3e59c76dac37e4e0d35e4bced
junaet faruk rafi <rafee790@gmail.com>
2024-01-09 01:08:56 +0600

    second commit

---
+++ ArithmeticOperators.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
+
+
+
+#Another Method
+
+# a = int(input())
+# b = int(input())
+# sum_ab = a + b
+# sub_ab = a - b
+# multi_ab = a * b
+# print(sum_ab)
+# print(sub_ab)
+# print(multi_ab)
